1d376d12fa811c8ec1e5b1cc59c1b4d64355a4c1
Chanelle Moore <mymedicalbible@gmail.com>
2026-04-13 20:37:42 -0700

    square poloroid fix

--- src/pages/MorePage.tsx
+++ src/pages/MorePage.tsx
@@ -12,7 +12,7 @@ type PolaroidNavCardProps = {
   tapeClass: '' | 'more-polaroid__tape--rose'
   frameRotateClass: string
   ariaLabel: string
-  /** Square photo opening (classic square polaroid look). */
+  /** Entire polaroid frame is square (image area + caption fit inside). */
   squarePhoto?: boolean
 }
 
@@ -30,10 +30,18 @@ function PolaroidNavCard ({
     <Link
       to={to}
       aria-label={ariaLabel}
-      className="more-polaroid-link group relative block w-[min(44vw,180px)] shrink-0 outline-none transition-transform duration-200 focus-visible:ring-2 focus-visible:ring-rose-300/90 focus-visible:ring-offset-2 focus-visible:ring-offset-[var(--more-hub-paper)]"
+      className={`more-polaroid-link group relative block shrink-0 outline-none transition-transform duration-200 focus-visible:ring-2 focus-visible:ring-rose-300/90 focus-visible:ring-offset-2 focus-visible:ring-offset-[var(--more-hub-paper)] ${
+        squarePhoto
+          ? 'aspect-square w-[min(44vw,180px)]'
+          : 'w-[min(44vw,180px)]'
+      }`}
     >
       <div
-        className={`more-polaroid__frame ${frameRotateClass} relative rounded-[2px] border border-black/[0.06] bg-white shadow-[0_8px_20px_rgba(45,38,42,0.18),0_2px_8px_rgba(0,0,0,0.07)]`}
+        className={`more-polaroid__frame ${frameRotateClass} relative rounded-[2px] border border-black/[0.06] bg-white shadow-[0_8px_20px_rgba(45,38,42,0.18),0_2px_8px_rgba(0,0,0,0.07)] ${
+          squarePhoto
+            ? 'flex h-full min-h-0 flex-col overflow-hidden pt-2.5 px-2.5 pb-2'
+            : ''
+        }`}
       >
         <span
           className={`more-polaroid__tape ${tapeClass}`.trim()}
@@ -42,13 +50,17 @@ function PolaroidNavCard ({
         <div
           className={`more-polaroid__photo ${photoClass} ${
             squarePhoto
-              ? 'aspect-square h-auto min-h-0'
+              ? 'flex-1 min-h-0 !m-0 !min-h-0'
               : 'h-[100px] min-h-[100px]'
           }`}
         >
           <span className="more-polaroid__title !text-[0.85rem]">{title}</span>
         </div>
-        <div className="more-polaroid__caption-strip py-2 px-3 !min-h-0">
+        <div
+          className={`more-polaroid__caption-strip py-2 px-3 !min-h-0 ${
+            squarePhoto ? 'shrink-0' : ''
+          }`}
+        >
           <span className="more-polaroid__caption text-[0.7rem]">{caption}</span>
         </div>
       </div>
